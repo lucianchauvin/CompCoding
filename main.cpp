61dd03849da4f286a2cbd322e4132cf1d6282ed6
Lucian Chauvin <lucianchauvin@gmail.com>
2023-07-16 14:31:24 -0500

    yeah

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+
+#define dbg(x) std::cout << "==DEBUG==  " << #x << " = " << x << endl;
+#define sum(x) std::accumulate(x.begin(), x.end(), 0)
+#define sortLG(x) std::sort(x.begin(), x.end(), less<int>())
+#define sortGL(x) std::sort(x.begin(), x.end(), greater<int>())
+#define reverse(x) std::reverse(x.begin(), x.end())
+#define foreach(i, x) for (auto i : x)
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+
+int main(int argc, const char** argv) {
+    return 0;
+}
